ccc99c46d3b0a8a5b058574d6450e4221c0071aa
clameraarduino <cristian.lamera@gmail.com>
2026-05-08 09:01:57 +0200

    Add pin configuration

---
+++ pin_config.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// ===== DISPLAY PINS =====
+#define LCD_SDIO0 4
+#define LCD_SDIO1 5
+#define LCD_SDIO2 6
+#define LCD_SDIO3 7
+#define LCD_SCLK 38
+#define LCD_CS 12
+#define LCD_RESET 39
+#define LCD_WIDTH 466
+#define LCD_HEIGHT 466
+
+// ===== I2C PINS (Touch + Sensors) =====
+#define IIC_SDA 15
+#define IIC_SCL 14
+
+// ===== TOUCH PINS =====
+#define TP_INT 11
+#define TP_RESET 40
